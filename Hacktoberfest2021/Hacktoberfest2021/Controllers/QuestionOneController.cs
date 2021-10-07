63eda339795283c8bc01a9875a22eaf50e2910ea
Andrew Robson <robson.andrew@gmail.com>
2021-10-07 17:57:17 +1300

    QuestionOne Working

---
+++ Hacktoberfest2021/Hacktoberfest2021/Controllers/QuestionOneController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hacktoberfest2021.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuestionOneController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            int[] numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            IEnumerable<int> resultQuery =
+                from number in numbers
+                where number % 2 == 0
+                select number;
+            return resultQuery;
+        }
+    }
+}
