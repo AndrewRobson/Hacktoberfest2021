4f297076ed2ed8a01dc963aee65f63df223bb296
Andrew Robson <robson.andrew@gmail.com>
2021-10-07 17:57:03 +1300

    QuestionTwo working

---
+++ Hacktoberfest2021/Hacktoberfest2021/Controllers/QuestionTwoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hacktoberfest2021.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuestionTwoController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            int[] arr1 = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            var result = (from x in arr1
+                          group x by x into y
+                          select y);
+            var res = result.ToDictionary(item => item.Key, item=> item.Count());
+            //var dict = result.ToDictionary<int>();
+            return JsonConvert.SerializeObject(res);
+        }
+
+        // GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
